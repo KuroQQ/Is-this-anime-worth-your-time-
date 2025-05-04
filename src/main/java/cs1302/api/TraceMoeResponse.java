b0228dabb7d6c3f18f0c0dadb456e3a15de5eded
Omoikhefe Ebohon <ole34661@uga.edu>
2025-05-04 15:13:01 -0400

    tis done!

--- src/main/java/cs1302/api/TraceMoeResponse.java
+++ src/main/java/cs1302/api/TraceMoeResponse.java
@@ -1,17 +1,32 @@
 package cs1302.api;
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Represents the response from the trace.moe API.
+ */
 public class TraceMoeResponse {
+    /** Array of results returned from the API. */
     public Result[] result;
 
+    /**
+     * Represents a single result from the trace.moe API.
+     */
     public static class Result {
-      public String filename;
-      public int episode;
-      public double similarity;
-      public String image;
-      public String video;
-      public int anilist;
-      public String title_english;
-      public String title_romaji;
-      public String title_native;
-    } //Result
-} //TraceMoeResponse
+        public String filename;
+        public int episode;
+        public double similarity;
+        public String image;
+        public String video;
+        public int anilist;
+
+        @SerializedName("title_english")
+        public String titleEnglish;
+
+        @SerializedName("title_romaji")
+        public String titleRomaji;
+
+        @SerializedName("title_native")
+        public String titleNative;
+    } // Result
+} // TraceMoeResponse
